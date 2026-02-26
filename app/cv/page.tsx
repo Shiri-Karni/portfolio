d2787a8c092795ca7663a7e598a51d05d8437365
Shiri Karni <shiri@innovate.huji.ac.il>
2026-02-26 17:32:59 +0200

    fixed logo

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CVPage = () => (
     <div>
         <h2 className='middleHeadline'>My Resume</h2>
-        <img className="cv" src="/icons/cv.svg" alt="Resume Icon" />
+        <img className="cv" src="/icons/CV.svg" alt="Resume Icon" />
     </div>
 );
 
